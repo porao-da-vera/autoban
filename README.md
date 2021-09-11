1355e5f1f5ce7a1ce677346c3b63861c34db33b0
jordao <jordaofranca@gmail.com>
2021-09-11 05:22:45 +0200

    First commit

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # autoban
+
 auto ban usernames that contains some terms
+
+## to get the token:
+
+[Chat token generation](https://twitchapps.com/tmi/)
---
+++ widget.js
@@ -0,0 +1,32 @@
+window.addEventListener("onWidgetLoad", function (obj) {
+  const fieldData = obj.detail.fieldData;
+  const channel = obj.detail.channel.username;
+  console.log("init", obj);
+  const options = {
+    options: {
+      debug: true,
+    },
+    connection: {
+      secure: true,
+      reconnect: true,
+    },
+    identity: {
+      username: channel,
+      password: fieldData.token,
+    },
+    channels: [channel],
+  };
+  let client = new tmi.client(options);
+
+  client.connect(options).then(() => {
+    window.addEventListener("onEventReceived", function (wobj) {
+      if (wobj.detail.listener === "follower-latest") {
+        const username = wobj.detail.event.name;
+        const blackList = fieldData.terms.split(",");
+        if (blackList.find((term) => username.includes(term))) {
+          client.ban(channel, username, "bot safado");
+        }
+      }
+    });
+  });
+});
